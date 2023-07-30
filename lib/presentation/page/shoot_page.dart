7f9127492e240b933c2dbe58ee3eec7cd4ed63b3
D'Angelo <63720046+d-angelo-2049@users.noreply.github.com>
2023-07-30 22:17:50 +0900

    added top page (#3)
    Co-authored-by: DaiOhtsuka <dohtsuka@DaiOhtsukanoMacBook-Air.local>

---
+++ lib/presentation/page/shoot_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ShootPage extends StatefulWidget {
+  const ShootPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    throw UnimplementedError();
+  }
+}
---
+++ lib/presentation/page/top_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:rememberme/presentation/navigation/page_nagivation.dart';
+import 'package:rememberme/presentation/page/danger_list_page.dart';
+import 'package:rememberme/presentation/page/shoot_page.dart';
+
+class TopPage extends StatelessWidget {
+  const TopPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text('Food Loss Reduction Application'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              OutlinedButton(
+                  onPressed: () {
+                    PageNavigation.push(context, const ShootPage());
+                    print('navigate to shoot page');
+                  },
+                  child: const Text(
+                    '忘れそうなものを登録しよう',
+                    style: TextStyle(color: Colors.blue, fontSize: 20.0),
+                  )),
+              const SizedBox(
+                height: 80,
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  PageNavigation.push(context, const DangerListPage());
+                  print('navigate to danger list page');
+                },
+                child: const Text(
+                  'もうそろそろ食べないといけないもの',
+                  style: TextStyle(color: Colors.blue, fontSize: 20.0),
+                ),
+              ),
+            ],
+          ),
+        ));
+  }
+}
